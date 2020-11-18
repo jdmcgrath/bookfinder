e098a98f05078d9cef647249b0a693291b966945
Joseph McGrath <joseph.mcgrath@engineer.com>
2020-11-18 12:46:54 +0000

    Add: firebase compatibility

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { CollectionComponent } from './collection/collection.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+const routes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'collection', component: CollectionComponent },
+];
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBHX5tdlQEBUcwBoHnFdv3bWOgOy32JECs",
+  authDomain: "bookfinder-dafee.firebaseapp.com",
+  databaseURL: "https://bookfinder-dafee.firebaseio.com",
+  projectId: "bookfinder-dafee",
+  storageBucket: "bookfinder-dafee.appspot.com",
+  messagingSenderId: "449818902077",
+  appId: "1:449818902077:web:42417f75b822905aa63454"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SearchComponent, CollectionComponent],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule,
+    FontAwesomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { faBookmark, faStar } from '@fortawesome/free-solid-svg-icons';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire';
+
+interface GoogleBooksResponse {
+  items: any[];
+  kind: string;
+  totalItems: number;
+}
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
+})
+export class SearchComponent implements OnInit {
+  searchText: string;
+  books = [];
+  faBookmark = faBookmark;
+  faStar = faStar;
+  constructor(private http: HttpClient, private firestore: AngularFirestore) {}
+
+  ngOnInit(): void {}
+
+  handleSearch() {
+    const searchedAuthor = this.searchText;
+    return this.http
+      .get(
+        'https://www.googleapis.com/books/v1/volumes?q=inauthor=' +
+          searchedAuthor
+      )
+      .toPromise()
+      .then((response: GoogleBooksResponse) => {
+        this.books = response.items;
+        console.log(this.books);
+      });
+  }
+
+  handleSave(book) {
+    this.firestore.doc(`colleciton/${book.id}`).set(book, { merge: true });
+  }
+}
